9df412457806b7c2be9dfde4717eae81ecf68478
Kiril <kiril.abashkin@gmail.com>
2020-03-08 12:42:16 +0200

    updates

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const Telegraf = require('telegraf')
 const Telegram = require('telegraf/telegram')
 const { UserModel, TicketModel } = require('./models')
 const uuid = require('uuid')
+const Markup = require('telegraf/markup')
+const Extra = require('telegraf/extra')
+
+const actions = require('./actions')
+
+function timeConverter(UNIX_timestamp) {
+    return new Date(UNIX_timestamp * 1000).toLocaleTimeString('lt-LT')
+}
 
 Mongoose.connect('mongodb://localhost/', {
-    dbName: 'ketpazeidimai',
+    dbName: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -17,13 +27,23 @@ Mongoose.connect('mongodb://localhost/', {
 
 const telegram = new Telegram(process.env.BOT_TOKEN)
 const bot = new Telegraf(process.env.BOT_TOKEN, { channelMode: false })
+// bot.use(Telegraf.log())
 
-function timeConverter(UNIX_timestamp) {
-    return new Date(UNIX_timestamp * 1000).toLocaleTimeString('lt-LT')
-}
+// bot.on('text', ctx => {
+//     return ctx.reply(
+//         'Special buttons keyboard',
+//         Extra.markup(markup => {
+//             return markup
+//                 .resize()
+//                 .keyboard([
+//                     markup.contactRequestButton('Send contact'),
+//                     markup.locationRequestButton('Send location')
+//                 ])
+//         })
+//     )
+// })
 
 bot.command('start', async ctx => {
-    const about = await telegram.getMe()
     const { id, first_name, last_name } = ctx.message.from
     await UserModel.findOne(
         {
@@ -44,9 +64,8 @@ bot.command('start', async ctx => {
                         function(err, user) {
                             if (!err) {
                                 ctx.reply(
-                                    `Welcome to ${about.username} ${first_name}!`
+                                    `Welcome to KET_PAZEIDIMAI bot ${first_name}!`
                                 )
-                                console.log(user)
                             }
                         }
                     )
@@ -99,7 +118,7 @@ bot.command('ket', async (ctx, next) => {
                             `Automobilis ${plateNumber} jau šiandien registruotas ${time}`
                         )
                         if (photos.length) {
-                            telegram.sendPhoto(
+                            ctx.replyWithPhoto(
                                 chatId,
                                 photos[0].file_id,
                                 plateNumber
@@ -147,7 +166,15 @@ bot.on('location', async ctx => {
         const { location } = ctx.message
         await TicketModel.updateOne(
             { plateNumber: bot.context.valstybinis_numeris },
-            { location }
+            { location },
+            function(err, res) {
+                if (!err && res.ok == 1) {
+                    ctx.reply(
+                        `Lokacija įrašyta ${bot.context.valstybinis_numeris}`
+                    )
+                }
+                console.log(res)
+            }
         )
     } else {
         ctx.reply('Pradžiai įveskit /ket [automobilio valstybinis numeris]')
@@ -162,7 +189,16 @@ bot.on('photo', async ctx => {
         const link = await telegram.getFileLink(fileId)
         await TicketModel.updateOne(
             { plateNumber: bot.context.valstybinis_numeris },
-            { $push: { photos: { link, file_id: fileId } } }
+            { $push: { photos: { link, file_id: fileId } } },
+            function(err, res) {
+                if (!err && res.ok == 1) {
+                    ctx.reply(
+                        `✅ Nuotrauka įrašyta ${bot.context.valstybinis_numeris}`
+                    )
+                } else {
+                    ctx.reply(`Nepavyko įrašyti nuotraukos: ${err}`)
+                }
+            }
         )
     } else {
         ctx.reply('Pradžiai įveskit /ket [automobilio valstybinis numeris]')
@@ -177,16 +213,25 @@ bot.command('reports', async (ctx, next) => {
     if (user) {
         const tickets = user.tickets
         if (tickets.length) {
-            tickets.forEach(({ photos, plateNumber }) => {
+            tickets.forEach(async ({ photos, plateNumber }) => {
                 if (photos.length) {
-                    telegram.sendPhoto(
-                        userId,
+                    await ctx.replyWithPhoto(
                         photos[0].file_id,
-                        plateNumber,
-                        true
+                        Extra.markup(
+                            Markup.inlineKeyboard([
+                                Markup.callbackButton(
+                                    `♻️ Pakeisti ${plateNumber}`,
+                                    'Update report'
+                                ),
+                                Markup.callbackButton(
+                                    '❌ Pašalinti',
+                                    actions.REMOVE_REPORT
+                                )
+                            ])
+                        )
                     )
                 } else {
-                    telegram.sendMessage(userId, `No photo: ${plateNumber}`)
+                    await ctx.reply(`No photo: ${plateNumber}`)
                 }
             })
         } else {
@@ -197,26 +242,90 @@ bot.command('reports', async (ctx, next) => {
     }
 })
 
+bot.command('simple', ctx => {
+    return ctx.replyWithHTML(
+        '<b>Coke</b> or <i>Pepsi?</i>',
+        Extra.markup(Markup.keyboard(['Coke', 'Pepsi']))
+    )
+})
+
 bot.command('remove', async (ctx, next) => {
     const userId = ctx.message.from.id
     const user = await UserModel.findOne({ userId })
+
     if (user) {
         const id = user._id
-        await TicketModel.deleteMany(
-            {
-                user: Mongoose.Types.ObjectId(id)
-            },
-            function(err, res) {
-                if (!err) {
-                    user.tickets = []
-                    user.save()
-                    ctx.reply(`Removed all ${res.deletedCount} report(s)!`)
-                } else {
-                    ctx.reply(err)
-                }
-            }
-        )
+        const tickets = await TicketModel.find({
+            user: Mongoose.Types.ObjectId(id)
+        })
+
+        if (tickets.length) {
+            return ctx.replyWithHTML(
+                `<b> Remove all ${tickets.length} reports?</b>`,
+                Extra.markup(
+                    Markup.inlineKeyboard([
+                        Markup.callbackButton(
+                            `Panaikinti visus ${tickets.length} pranšimus`,
+                            'Remove all reports'
+                        ),
+                        Markup.callbackButton('Ne', 'No')
+                    ])
+                )
+            )
+        } else {
+            return ctx.reply('There is no reports')
+        }
     }
+})
+
+bot.action('No', async ctx => {
+    await ctx.answerCbQuery()
+    await ctx.reply('Gerai 👌')
+})
+
+bot.action('Remove all reports', async ctx => {
+    return ctx
+        .answerCbQuery()
+        .then(async res => {
+            const userId = ctx.update.callback_query.message.chat.id
+            const user = await UserModel.findOne({ userId })
+            const id = user._id
+            return await TicketModel.deleteMany(
+                {
+                    user: Mongoose.Types.ObjectId(id)
+                },
+                function(err, res) {
+                    if (!err) {
+                        user.tickets = []
+                        user.save()
+                        ctx.reply(`Removed all ${res.deletedCount} report(s)!`)
+                    } else {
+                        ctx.reply(err)
+                    }
+                }
+            )
+        })
+        .catch(err => ctx.reply(err))
+})
+
+bot.action('Update report', async ctx => {
+    return ctx
+        .answerCbQuery()
+        .then(res => {
+            console.log(ctx.update, ctx.update.callback_query.message.chat.id)
+            return ctx.reply(res)
+            // const userId = ctx.update.callback_query.message.chat.id
+            // const user = await UserModel.findOne({ userId })
+        })
+        .catch(err => ctx.reply(err))
+})
+
+bot.on('contact', async ctx => {
+    console.log(ctx)
+})
+
+bot.action(actions.REMOVE_REPORT, async ctx => {
+    console.log(ctx)
 })
 
 bot.launch()
