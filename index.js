134c56ee2a2608a873fe748aa5494952e27774f8
reskir <kiril.abashkin@gmail.com>
2021-04-18 11:53:20 +0300

    minor updates

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 import fetch from 'node-fetch'
 import Mongoose from 'mongoose'
-import { Telegraf, Markup, Telegram } from 'telegraf'
+import { Telegraf, Telegram } from 'telegraf'
 import LocalSession from 'telegraf-session-local'
-import { UserModel, TicketModel } from './models'
-import {
-    getAllUserTickets,
-    updateTicket,
-    getStatusMessage,
-} from './helpers/helpers'
-import { registerKet } from './src/commands/ket'
-import { addPhoto } from './src/commands/addPhoto'
-import fs from 'fs'
+import { UserModel } from './models'
 import request from 'request'
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
@@ -25,7 +17,6 @@ if (!BOT_TOKEN) {
 }
 
 const telegram = new Telegram(BOT_TOKEN)
-
 const bot = new Telegraf(BOT_TOKEN, { channelMode: false })
 
 bot.use(new LocalSession({ database: 'example_db.json' }).middleware())
@@ -85,10 +76,6 @@ bot.command('start', async (ctx) => {
     }
 })
 
-bot.command('ket', async (ctx, next) => {
-    return await registerKet({ ctx, bot })
-})
-
 bot.command('login', async (ctx) => {
     ctx.reply('Login')
     const response = await fetch(
@@ -122,7 +109,7 @@ bot.command('reset', (ctx) => {
     if (ctx?.session?.issue) {
         ctx.replyWithMarkdown(
             `Removing session from database: \`${JSON.stringify(
-                ctx.session.issue.name
+                ctx.session.issue
             )}\``
         )
         // Setting session to null, undefined or empty object/array will trigger removing it from database
@@ -183,8 +170,16 @@ bot.command('current', (ctx) => {
         if (ctx.session?.images) {
             const images = ctx.session?.images
             for (const image of images) {
-                console.log(image)
-                ctx.replyWithPhoto(image)
+                if (image) {
+                    request(
+                        { url: image, encoding: null },
+                        (err, resp, buffer) => {
+                            if (resp) {
+                                ctx.replyWithPhoto(buffer)
+                            }
+                        }
+                    )
+                }
             }
         }
     } else {
@@ -213,12 +208,9 @@ bot.command('photo', async (ctx) => {
 })
 
 bot.on('callback_query', async (ctx) => {
-    //console.log(ctx.update?.callback_query?.data)
     const data = JSON.parse(ctx?.update?.callback_query?.data)
-    console.log(data)
     if (data) {
         const issue = ctx?.issues.find(({ id }) => id === data?.issueId)
-        console.log(ctx.issues)
         ctx.session.issue = issue.name
         ctx.session.issueId = data?.issueId
         ctx.reply(`Pasirinkote: ${issue.name}`)
@@ -246,302 +238,21 @@ bot.on(['document', 'photo'], async (ctx) => {
         const file_id = ctx.update.message.document.file_id
         const { file_path } = await telegram.getFile(file_id)
         const fileURL = `https://api.telegram.org/file/bot${BOT_TOKEN}/${file_path}`
-        request({ url: fileURL, encoding: null }, (err, resp, buffer) => {
-            if (!ctx.session?.images) {
-                ctx.session.images = [buffer.toString('base64')]
-            } else {
-                ctx.session.images.push(buffer.toString('base64'))
-            }
-        })
-    }
 
-    //await addPhoto({ ctx, bot, telegram })
-})
-
-bot.on('location', async (ctx) => {
-    if (bot.context.uniqueId) {
-        const { location } = ctx.message
-        await updateTicket(
-            bot.context.uniqueId,
-            { location: location },
-            async (err, res) => {
-                if (!err && res.ok === 1) {
-                    const { latitude, longitude } = location
-                    const url = `https://geocode.xyz/${latitude},${longitude}?json=1`
-                    await fetch(url)
-                        .then((response) => {
-                            return response.json()
-                        })
-                        .then(async (data) => {
-                            const address = `${data.staddress}. ${data.stnumber}`
-                            await updateTicket(
-                                bot.context.uniqueId,
-                                { location: { ...location, address } },
-                                async (err, res) => {
-                                    if (!err && res.ok === 1) {
-                                        ctx.reply(
-                                            `Lokacija įrašyta ${bot.context.valstybinis_numeris}`
-                                        )
-                                    } else if (err) {
-                                        ctx.reply(err)
-                                    }
-                                }
-                            )
-                        })
-                        .catch((err) => ctx.reply(err))
-                } else if (err) {
-                    console.log(err)
-                }
-            }
-        )
-    } else {
-        ctx.reply('Pradžiai įveskit /ket [automobilio valstybinis numeris]')
-    }
-})
-
-bot.command('reports', async (ctx) => {
-    const userId = ctx.message.from.id
-    const user = await UserModel.findOne({
-        userId: userId,
-    })
-    if (user) {
-        const tickets = await getAllUserTickets(user)
-        if (tickets.length) {
-            tickets.forEach(
-                async ({
-                    _id,
-                    photos,
-                    plateNumber,
-                    time,
-                    date,
-                    location: { address = 'Nėra' },
-                    currentStatus: { status },
-                }) => {
-                    if (photos.length) {
-                        const isDocument = photos[0].isDocument
-                        const replyOption = isDocument
-                            ? 'replyWithDocument'
-                            : 'replyWithPhoto'
-                        await ctx[replyOption](
-                            { source: photos[0].file_id },
-                            {
-                                caption: getStatusMessage({
-                                    plateNumber,
-                                    time,
-                                    address,
-                                    date,
-                                    status,
-                                }),
-                            }
-                                .markdown()
-                                .markup((m) => {
-                                    if (status === 'laukiama patvirtinimo') {
-                                        return m.inlineKeyboard([
-                                            m.callbackButton(
-                                                `♻️ Pakeisti ${plateNumber}`,
-                                                `UPDATE REPORT ${_id}`
-                                            ),
-                                            m.callbackButton(
-                                                '❌ Pašalinti',
-                                                `REMOVE REPORT ${_id}`
-                                            ),
-                                        ])
-                                    }
-                                })
-                        ).catch((e) => ctx.reply(e))
-                    } else {
-                        await ctx.reply(
-                            getStatusMessage({
-                                plateNumber,
-                                time,
-                                address,
-                                date,
-                                status,
-                            }),
-                            status === 'laukiama patvirtinimo'
-                                ? Markup.inlineKeyboard([
-                                      Markup.callbackButton(
-                                          `♻️ Pakeisti ${plateNumber}`,
-                                          `UPDATE REPORT ${_id}`
-                                      ),
-                                      Markup.callbackButton(
-                                          '❌ Pašalinti',
-                                          `REMOVE REPORT ${_id}`
-                                      ),
-                                  ])
-                                : null
-                        )
-                    }
-                }
-            )
+        if (!ctx.session?.images) {
+            ctx.session.images = [fileURL]
         } else {
-            ctx.reply(
-                '🔍 Pranešimų nerasta, pradėkime registruoti pranešima su /ket [AUTOMOBILIO VALSTYBINIS NUMERIS] komanda'
-            )
+            ctx.session.images.push(fileURL)
         }
-    } else {
-        ctx.reply('Vartotojas nerastas.')
+        // request({ url: fileURL, encoding: null }, (err, resp, buffer) => {
+        //     if (!ctx.session?.images) {
+        //         ctx.session.images = [fileURL]
+        //     } else {
+        //         ctx.session.images.push(fileURL)
+        //     }
+        // })
     }
 })
-
-bot.command('remove', async (ctx, next) => {
-    const userId = ctx.message.from.id
-    const user = await UserModel.findOne({ userId })
-
-    if (user) {
-        const tickets = await getAllUserTickets(user)
-
-        if (tickets.length) {
-            return ctx.replyWithHTML(
-                `<b> Remove all ${tickets.length} reports?</b>`,
-                Markup.inlineKeyboard([
-                    Markup.callbackButton(
-                        `Panaikinti visus ${tickets.length} pranšimus`,
-                        'Remove all reports'
-                    ),
-                    Markup.callbackButton('Ne', 'No'),
-                ]).oneTime()
-            )
-        } else {
-            return ctx.reply('Pranešimų nėra.')
-        }
-    }
-})
-
-bot.action('No', async (ctx) => {
-    await ctx.answerCbQuery()
-    await ctx.reply('Gerai 👌')
-})
-
-bot.action('Remove all reports', async (ctx) => {
-    return ctx
-        .answerCbQuery()
-        .then(async (res) => {
-            const userId = ctx.update.callback_query.message.chat.id
-            const user = await UserModel.findOne({ userId }).populate('tickets')
-            const id = user._id
-            const tickets = user.tickets
-            tickets.forEach(async ({ _id }) => {
-                await fs.rmdir(`files/${_id}`, { recursive: true }, (err) =>
-                    console.log(err)
-                )
-            })
-            return await TicketModel.deleteMany(
-                {
-                    user: Mongoose.Types.ObjectId(id),
-                },
-                function (err, res) {
-                    if (!err) {
-                        user.tickets = []
-                        user.save()
-                        ctx.reply(`Removed all ${res.deletedCount} report(s)!`)
-                    } else {
-                        ctx.reply(err)
-                    }
-                }
-            )
-        })
-        .catch((err) => ctx.reply(err))
-})
-
-bot.action(/\UPDATE REPORT +.*/, async (ctx) => {
-    const id = ctx.update.callback_query.data.replace('UPDATE REPORT ', '')
-    const userId = ctx.update.callback_query.message.chat.id
-    const user = await UserModel.findOne({ userId }).populate('tickets')
-    const isAlreadyRegistered = user.tickets.find(
-        ({ _id }) => _id.toString() === id
-    )
-    const isStatusWaiting = user.tickets.find(
-        ({ currentStatus: { status } }) => status === 'laukiama patvirtinimo'
-    )
-    const { plateNumber } = isAlreadyRegistered
-    if (isAlreadyRegistered && isStatusWaiting) {
-        bot.context.uniqueId = id
-        bot.context.valstybinis_numeris = plateNumber
-        ctx.reply(
-            `✅ Galite atnaujinti (pridėti lokaciją, nuotraukas) ${plateNumber}`
-        )
-    } else if (!isStatusWaiting) {
-        ctx.reply(`Pranešimas ${plateNumber} negali būti keičiamas.`)
-    } else {
-        ctx.reply(`🔍 Pranešimas ${plateNumber} nerastas.`)
-    }
-})
-
-bot.action(/\REMOVE REPORT +.*/, async (ctx) => {
-    const id = ctx.update.callback_query.data.replace('REMOVE REPORT ', '')
-    const userId = ctx.update.callback_query.message.chat.id
-    const user = await UserModel.findOne({ userId }).populate('tickets')
-    const isAlreadyRegistered = user.tickets.find(
-        ({ _id }) => _id.toString() === id
-    )
-
-    const isStatusWaiting = user.tickets.find(
-        ({ currentStatus: { status } }) => status === 'laukiama patvirtinimo'
-    )
-    if (user && isStatusWaiting) {
-        await TicketModel.deleteOne(
-            {
-                _id: id,
-                user: Mongoose.Types.ObjectId(user._id),
-            },
-            async function (err, res) {
-                if (!err && isAlreadyRegistered && isStatusWaiting) {
-                    const { plateNumber } = isAlreadyRegistered
-                    await fs.rmdir(`files/${id}`, { recursive: true }, (err) =>
-                        console.log(err)
-                    )
-
-                    user.tickets = user.tickets.filter(
-                        ({ _id }) => _id.toString() !== id
-                    )
-                    user.save()
-                    const messageId =
-                        ctx.update.callback_query.message.message_id
-                    ctx.deleteMessage(messageId)
-                    ctx.reply(`${plateNumber} sėkmingai pašalintas ✅`)
-                } else if (!isAlreadyRegistered && !err) {
-                    ctx.reply(
-                        `🔍 Pranešimas nerastas. Turbut jis jau buvo pašalintas?`
-                    )
-                } else {
-                    ctx.reply(err)
-                }
-            }
-        )
-    } else if (!isStatusWaiting) {
-        ctx.reply(`Pranešimas negali būti keičiamas.`)
-    }
-})
-
-bot.hears(
-    [
-        /(\d{1,2})+\:+(\d{1,2})/,
-        /\d{4}.\d{1,2}.\d{1,2}/,
-        /\d{4}.\d{1,2}.\d{1,2} \d{1,2}.\d{1,2}/,
-    ],
-    async (ctx) => {
-        if (bot.context.uniqueId) {
-            const time = ctx?.update?.message?.text
-            await updateTicket(
-                bot.context.uniqueId,
-                { time: time },
-                async (err, res) => {
-                    if (!err && res) {
-                        ctx.reply(
-                            `Laikas atnaujintas ${time} 🕜 ${bot.context.valstybinis_numeris}`
-                        )
-                    }
-                    if (err) {
-                        ctx.reply(err)
-                    }
-                }
-            )
-        } else {
-            ctx.reply('Valstybinio numerio nėra')
-        }
-    }
-)
 
 bot.launch()
 
